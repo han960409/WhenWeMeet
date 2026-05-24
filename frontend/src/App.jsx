b3cf5fe6571918b705005797a4f5ee692dc51c8c
han96 <han960409@naver.com>
2026-05-24 10:23:30 +0900

    123

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -168,7 +168,7 @@ function LoginPage() {
             className="google-login-btn"
             onClick={() => {
               window.location.href =
-                `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`;
+                `${API_BASE_URL}/oauth2/authorization/google`;
             }}
           >
             <img
